9a7eb22b0c0b830734698af419871c945bf8916e
Aaron Durant <aaron@aarondurant.com>
2022-08-02 11:49:25 -0400

    Build hero section

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,7 +17,7 @@ const Navbar = ({ toggle }) => {
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to="/">BrandCop.</NavLogo>
+          <NavLogo to="/">Brand.</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
