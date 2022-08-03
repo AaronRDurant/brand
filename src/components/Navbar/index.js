63020a9bf2c56486a83e4d61c259bc6b57a01d40
Aaron Durant <aaron@aarondurant.com>
2022-08-03 11:37:00 -0400

    Build footer

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -17,7 +17,7 @@ const Navbar = ({ toggle }) => {
     <>
       <Nav>
         <NavbarContainer>
-          <NavLogo to="/">Brand.</NavLogo>
+          <NavLogo to="/">brand.</NavLogo>
           <MobileIcon onClick={toggle}>
             <FaBars />
           </MobileIcon>
